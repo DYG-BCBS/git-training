86ad4ad0b09dfb569dae58644f79f5182ce62c67
Daniel Gu <Daniel.Gu@bcbsma.com>
2019-03-18 14:33:03 -0400

    add  de
    git status

---
+++ date-today.py
@@ -0,0 +1,14 @@
+# This file has a basic function
+
+import datetime
+
+def print_time():
+    print(f"Today is (datetime.dateitme.now())")
+
+def main():
+    print_time()
+
+# runs program like a script
+if __name__ == '__main__':
+    main()
+
